43e1da69b22c98ec5d0443f0b7bf34d6fc53b5e4
zlsun <zlsun1995@gmail.com>
2015-06-11 19:14:58 +0800

    update

--- umake.plugin.zsh
+++ umake.plugin.zsh
@@ -1,7 +1,14 @@
 
-autoload -Uz bashcompinit compinit
-compinit
+autoload -U bashcompinit
 bashcompinit -i
 
-eval "$(register-python-argcomplete umake)"
+_python_argcomplete() {
+    local IFS=''
+    COMPREPLY=( $(IFS="$IFS"                   COMP_LINE="$COMP_LINE"                   COMP_POINT="$COMP_POINT"                   _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS"                   _ARGCOMPLETE=1                   "$1" 8>&1 9>&2 1>/dev/null 2>/dev/null) )
+    if [[ $? != 0 ]]; then
+        unset COMPREPLY
+    fi
+}
+
+complete -o nospace -o default -F _python_argcomplete "umake"
 
